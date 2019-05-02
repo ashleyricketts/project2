03382c0e53a8fe0f0540725456464cede22fb30f
Ashley Ricketts <aricketts12@gmail.com>
2019-05-01 20:03:11 -0500

    schema.db edit database and tables created

--- Database/schema.sql
+++ Database/schema.sql
@@ -1,5 +1,39 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+CREATE DATABASE petPartyDB;
+USE petPartyDB;
+
+CREATE TABLE users
+(
+    userid int NOT NULL AUTO_INCREMENT,
+    username varchar(30) NOT NULL,
+    userphoto varchar(255),
+    email varchar(30) NOT NULL,
+    dog BOOLEAN default false,
+    cat BOOLEAN default false,
+    bird BOOLEAN default false,
+    reptile BOOLEAN default false,
+    fish BOOLEAN default false,
+    multi BOOLEAN default false, 
+    petNames varchar(255),
+    PRIMARY KEY (userid)
+);
+
+CREATE TABLE events
+(
+    eventid int NOT NULL AUTO_INCREMENT,
+    title varchar(180) NOT NULL,
+    link varchar(255),
+    address varchar(255) NOT NULL,
+    dog BOOLEAN default false,
+    cat BOOLEAN default false,
+    bird BOOLEAN default false,
+    reptile BOOLEAN default false,
+    fish BOOLEAN default false,
+    multi BOOLEAN default false,
+    PRIMARY KEY (eventid)
+
+);
+
+
+
+
