a8862220c0d01b431c9c36c23cae9cd40f531f10
Ashley Ricketts <aricketts12@gmail.com>
2019-05-04 14:26:18 -0500

    added events.js file for events model

--- Database/schema.sql
+++ Database/schema.sql
@@ -1,33 +1,7 @@
-
+DROP DATABASE IF EXISTS petPartyDB;
 CREATE DATABASE petPartyDB;
 USE petPartyDB;
 
-CREATE TABLE users
-(
-    userid int NOT NULL AUTO_INCREMENT,
-    username varchar(30) NOT NULL,
-    userphoto varchar(255),
-    email varchar(30) NOT NULL,
-    -- pet column will collect: dog, cat, bird, fish, reptile from dropdown or checklist
-    pet varchar(30) NOT NULL, 
-    petNames varchar(255),
-    PRIMARY KEY (userid)
-);
-
-CREATE TABLE events
-(
-    eventid int NOT NULL AUTO_INCREMENT,
-    title varchar(180) NOT NULL,
-    date varchar(25) not null,
-    time varchar(25) not null,
-    link varchar(255),
-    address varchar(255) NOT NULL,
-    -- pet column will collect: dog, cat, bird, fish, reptile from dropdown or checklist
-    pet varchar(30) NOT NULL,
-    hostname varchar(255),
-    PRIMARY KEY (eventid)
-
-);
 
 
 
--- Database/seeds.sql
+++ Database/seeds.sql
@@ -1,7 +1,8 @@
-INSERT INTO users (username, userphoto, email, pet, petnames) VALUES('TheCatLady', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFjM_jrux0vsHSmqOr_LSB8sK8GZRaxoCOmneMxUu-fh8N4PZc', 'thecatlady@aol.com', "dog", 'FluffyMuffins Bruno LadyCrumpetts');
-INSERT INTO users (username, userphoto, email, pet, petnames) VALUES('TheDogDude', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyzkyovR80FZgW2_ZFv5yl2ZMH287YijSj7KOkhfXhitGuSbgOCg', 'thedogdude@aol.com', "dog", 'Flash Duke Ace');
-INSERT INTO users (username, userphoto, email, pet, petnames) VALUES('ReptileLovinAussie', 'https://images2.minutemediacdn.com/image/upload/c_crop,h_842,w_1500,x_0,y_52/f_auto,q_auto,w_1100/v1554701032/shape/mentalfloss/gettyimages-1129399.jpg', 'stevesthename@aol.com', "reptile", 'Kipper Nipper Ripper');
+--stock rows for testing purposes
+INSERT INTO users (username, userphoto, email, petTypes, petnames) VALUES('TheCatLady', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFjM_jrux0vsHSmqOr_LSB8sK8GZRaxoCOmneMxUu-fh8N4PZc', 'thecatlady@aol.com', "dog", 'FluffyMuffins Bruno LadyCrumpetts');
+INSERT INTO users (username, userphoto, email, petTypes, petnames) VALUES('TheDogDude', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyzkyovR80FZgW2_ZFv5yl2ZMH287YijSj7KOkhfXhitGuSbgOCg', 'thedogdude@aol.com', "dog", 'Flash Duke Ace');
+INSERT INTO users (username, userphoto, email, petTypes, petnames) VALUES('ReptileLovinAussie', 'https://images2.minutemediacdn.com/image/upload/c_crop,h_842,w_1500,x_0,y_52/f_auto,q_auto,w_1100/v1554701032/shape/mentalfloss/gettyimages-1129399.jpg', 'stevesthename@aol.com', "reptile", 'Kipper Nipper Ripper');
 
 
-INSERT INTO events (title, date, time, link, address, pet, hostname) VALUES('Tea w/ FluufyMuffins', '05/30/19', '02:00pm', 'https://www.mewsickittycafe.com/', '2519 Nolensville Pike, Nashville, TN', "cat",'TheCatLady')
-INSERT INTO events (title, date, time, link, address, pet, hostname) VALUES('B-day party for Duke', '06/01/19', '03:00pm', 'https://www.bringfido.com/attraction/3483', '401 20th Ave S, Nashville, TN', "dog",'TheDogDude')
+INSERT INTO events (title, date, time, link, address, petTypes, hostname) VALUES('Tea w/ FluufyMuffins', '05/30/19', '02:00pm', 'https://www.mewsickittycafe.com/', '2519 Nolensville Pike, Nashville, TN', "cat",'TheCatLady')
+INSERT INTO events (title, date, time, link, address, petTypes, hostname) VALUES('B-day party for Duke', '06/01/19', '03:00pm', 'https://www.bringfido.com/attraction/3483', '401 20th Ave S, Nashville, TN', "dog",'TheDogDude')
