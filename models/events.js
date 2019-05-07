a04b2659815832ed976e618851d9f6a2d8f5d0f6
Bosco <bosco@MacBook-Air.hsd1.tn.comcast.net>
2019-05-07 17:40:29 -0500

    worked around routes, index.js, and models

--- models/events.js
+++ models/events.js
@@ -1,6 +1,5 @@
 module.exports = function(sequelize, DataTypes) {
   var Events = sequelize.define("Events", {
-    event_id: DataTypes.INTEGER,
     title: DataTypes.STRING,
     date: DataTypes.DATE,
     time: DataTypes.TIME,
@@ -13,9 +12,9 @@ module.exports = function(sequelize, DataTypes) {
   Events.associate = function(models) {
     //event should belong to a user
     Events.belongsTo(models.Users, {
-      foreignKey: {
-        allowNull: false
-      }
+      //foreignKey: {
+       // allowNull: false
+     // }
     });
   };
 
--- models/users.js
+++ models/users.js
@@ -1,6 +1,5 @@
 module.exports = function(sequelize, DataTypes) {
   var Users = sequelize.define("Users", {
-    user_id: DataTypes.INTEGER,
     user_name: DataTypes.TEXT,
     user_photo: DataTypes.TEXT,
     email: DataTypes.STRING,
